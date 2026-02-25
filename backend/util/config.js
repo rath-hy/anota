4909c71f36bfb0b89f0364cffb73ba602f132df7
rath hy <puthyrathy@gmail.com>
2026-02-24 16:12:33 -0800

    fix backend to work with domain

--- backend/util/config.js
+++ backend/util/config.js
@@ -12,5 +12,5 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: process.env.PORT || 3001,
   SECRET: process.env.SECRET,
-  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173'
+  FRONTEND_URL: (process.env.FRONTEND_URL || 'http://localhost:5173').split(',')
 }
