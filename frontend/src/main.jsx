8900dd3a51018a9fe00999e270fc09e0ac30e686
rath hy <puthyrathy@gmail.com>
2026-02-24 17:55:38 -0800

    fix font issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,8 +9,8 @@ import userReducer from './reducers/userReducer.jsx'
 import { createStore, combineReducers } from 'redux'
 
 import store from './store/index.js'
-import '@fontsource/libre-caslon-text/400.css'
-import '@fontsource/libre-caslon-text/700.css'
+import '@fontsource/eb-garamond/400.css'
+import '@fontsource/eb-garamond/700.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
